f88d63b0a598335525cd8ef7063f29f7732a9e85
ttowntom <git.running054@passinbox.com>
2024-11-15 14:26:31 +0100

    corrects styling

--- src/components/Callout.jsx
+++ src/components/Callout.jsx
@@ -1,7 +1,7 @@
 export default function Callout({ children }) {
   return (
     <section className="bg-navy h-6 min-h-fit py-16 text-white">
-      <div className="mx-auto flex w-full max-w-screen-lg flex-col justify-center px-4 pb-4 sm:flex-row">
+      <div className="text-blue-zodiac-50 mx-auto flex w-full max-w-screen-lg flex-col justify-center px-4 pb-4">
         {children}
       </div>
     </section>
