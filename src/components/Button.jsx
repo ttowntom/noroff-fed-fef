bd109ffe8281a2811a10d80512a113223137a684
ttowntom <git.running054@passinbox.com>
2024-11-15 13:38:02 +0100

    adds hover styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 export default function Button({ style, children }) {
-  let styling = 'border-navy rounded border px-4 py-2';
+  let styling = 'border-navy rounded border px-4 py-2 hover:bg-cat';
 
   if (style === 'primary') {
-    styling += ' bg-navy text-blue-zodiac-50';
+    styling += ' bg-navy text-blue-zodiac-50 hover:text-navy';
   }
 
   return <button className={styling}>{children}</button>;
