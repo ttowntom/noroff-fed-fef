8efc40d1b70bc461f331b105cf44ca7737c46788
ttowntom <git.running054@passinbox.com>
2024-11-15 11:20:41 +0100

    adds footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,50 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { byPrefixAndName } from '@awesome.me/kit-8d12afa6e5/icons';
+
 export default function Footer() {
   return (
-    <div className="mx-auto flex w-full max-w-screen-lg px-4 pb-4">
-      <h2>Footer</h2>
-    </div>
+    <footer className="mx-auto flex w-full max-w-screen-lg flex-col justify-center px-4 pb-4">
+      <p className="font-pacifico mb-10 text-center text-xl">
+        <span className="mr-2">
+          <FontAwesomeIcon icon={byPrefixAndName.fas['store']} />
+        </span>
+        Shoplyst
+      </p>
+      <ul className="gap-6 text-center font-medium sm:mb-4 sm:flex sm:flex-row sm:justify-center">
+        <li className="hover:underline">
+          <Link to="/">Shop Now</Link>
+        </li>
+        <li className="hover:underline">
+          <Link to="/contact">Contact Us</Link>
+        </li>
+        <li className="hover:underline">
+          <Link to="/">About Us</Link>
+        </li>
+        <li className="hover:underline">
+          <Link to="/">FAQs</Link>
+        </li>
+        <li className="hover:underline">
+          <Link to="/">Gift Cards</Link>
+        </li>
+      </ul>
+      <span className="border-navy my-8 h-1 w-full border-b"></span>
+      <div className="flex flex-col-reverse justify-between text-center text-sm sm:mb-6 sm:flex-row">
+        <p className="my-6 sm:my-0">
+          &copy; 2024 Shoplyst. All rights reserved.
+        </p>
+        <ul className="flex flex-col gap-4 sm:flex-row">
+          <li className="underline hover:no-underline">
+            <Link to="/">Privacy Policy</Link>
+          </li>
+          <li className="underline hover:no-underline">
+            <Link to="/">Terms of Use</Link>
+          </li>
+          <li className="underline hover:no-underline">
+            <Link to="/">Cookie Policy</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 }
