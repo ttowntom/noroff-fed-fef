cd8f351479b1cf38bdddd8773b4c3cb50db1756f
ttowntom <git.running054@passinbox.com>
2024-11-18 13:45:40 +0100

    fetch product details

--- src/util/http.js
+++ src/util/http.js
@@ -19,3 +19,19 @@ export async function getProducts() {
     throw error;
   }
 }
+
+export async function getProductById(id) {
+  try {
+    const response = await fetch(`${url}/${id}`);
+
+    if (!response.ok) {
+      throw new Error(`${response?.message || 'Failed to fetch product data'}`);
+    }
+
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
