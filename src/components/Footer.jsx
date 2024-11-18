ac7c9fb78352d477e5fbb9a85a41b09f379b50b4
ttowntom <git.running054@passinbox.com>
2024-11-18 11:27:06 +0100

    changes styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { byPrefixAndName } from '@awesome.me/kit-8d12afa6e5/icons';
 export default function Footer() {
   return (
     <footer className="mx-auto flex w-full max-w-screen-lg flex-col justify-center px-4 pb-4 pt-12">
-      <p className="font-pacifico mb-10 text-center text-xl">
+      <p className="mb-10 text-center font-pacifico text-xl">
         <span className="mr-2">
           <FontAwesomeIcon icon={byPrefixAndName.fas['store']} />
         </span>
@@ -28,7 +28,7 @@ export default function Footer() {
           <Link to="/">Gift Cards</Link>
         </li>
       </ul>
-      <span className="border-navy my-8 h-1 w-full border-b"></span>
+      <span className="my-8 h-1 w-full border-b border-navy"></span>
       <div className="flex flex-col-reverse justify-between text-center text-sm sm:mb-6 sm:flex-row">
         <p className="my-6 sm:my-0">
           &copy; 2024 Shoplyst. All rights reserved.
