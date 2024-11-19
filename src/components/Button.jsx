6892a7ae1b10e9e467d30ec815f507c11c268c3d
ttowntom <git.running054@passinbox.com>
2024-11-19 12:33:25 +0100

    accepts inverse styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-export default function Button({ style, grow, children }) {
+export default function Button({ style, grow, inverse, children }) {
   let styling = 'border-navy rounded border px-4 py-2 hover:bg-cat ';
 
   if (style === 'primary') {
@@ -7,6 +7,11 @@ export default function Button({ style, grow, children }) {
 
   if (grow) {
     styling += ' w-full';
+  }
+
+  if (inverse) {
+    styling +=
+      ' bg-navy border border-white text-white hover:bg-white hover:text-navy';
   }
 
   return <button className={styling}>{children}</button>;
