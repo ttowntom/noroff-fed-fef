621eaa19c029aae44966d0abe2b9e6e56241626c
ttowntom <git.running054@passinbox.com>
2024-11-19 11:32:19 +0100

    option to grow full width

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
-export default function Button({ style, children }) {
-  let styling = 'border-navy rounded border px-4 py-2 hover:bg-cat';
+export default function Button({ style, grow, children }) {
+  let styling = 'border-navy rounded border px-4 py-2 hover:bg-cat ';
 
   if (style === 'primary') {
     styling += ' bg-navy text-blue-zodiac-50 hover:text-navy';
+  }
+
+  if (grow) {
+    styling += ' w-full';
   }
 
   return <button className={styling}>{children}</button>;
