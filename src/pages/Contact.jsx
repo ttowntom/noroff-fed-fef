f8b61f18f7a3d7b0c9244cc3bfcadc350dbd2f22
ttowntom <git.running054@passinbox.com>
2024-11-20 11:15:39 +0100

    adds success message and return button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { z } from 'zod';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,9 +8,11 @@ import contactSchema from '../schemas/contactSchema';
 import Button from '../components/Button';
 
 export default function Contact() {
+  const navigate = useNavigate();
   const [formErrors, setFormErrors] = useState({});
   const [formData, setFormData] = useState({});
   const [touched, setTouched] = useState({});
+  const [msgSent, setMsgSent] = useState(false);
 
   const validateField = (name, value) => {
     try {
@@ -48,6 +51,8 @@ export default function Contact() {
     try {
       contactSchema.parse(formData);
       setFormErrors({});
+      setFormData(formData);
+      setMsgSent(true);
       console.log('Form is valid: ', formData);
     } catch (error) {
       if (error instanceof z.ZodError) {
@@ -59,6 +64,10 @@ export default function Contact() {
         console.error('Validation errors:', error.errors);
       }
     }
+  };
+
+  const handleNavigation = () => {
+    navigate('/#shop');
   };
 
   return (
@@ -74,99 +83,116 @@ export default function Contact() {
           <p>We'd love to hear from you. Reach out today!</p>
         </div>
         {/* Form */}
-        <form
-          onSubmit={handleSubmit}
-          className="mx-auto my-16 w-full max-w-[50ch]"
-        >
-          <div className="flex w-full flex-col gap-4">
-            <div className="flex flex-col gap-2">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="John Doe"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                className={`rounded border p-2 ${
-                  touched.name && formErrors.name
-                    ? 'border-error bg-error-light' // Show error styling only if the field is touched
-                    : touched.name && !formErrors.name
-                      ? 'border-success bg-success-light' // Show success styling if touched and valid
-                      : 'border-navy'
-                }`}
-              />
-              {formErrors.name && (
-                <p className="text-error">{formErrors.name}</p>
-              )}
+        {!msgSent && (
+          <form
+            onSubmit={handleSubmit}
+            className="mx-auto my-16 w-full max-w-[50ch]"
+          >
+            <div className="flex w-full flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="John Doe"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  className={`rounded border p-2 ${
+                    touched.name && formErrors.name
+                      ? 'border-error bg-error-light' // Show error styling only if the field is touched
+                      : touched.name && !formErrors.name
+                        ? 'border-success bg-success-light' // Show success styling if touched and valid
+                        : 'border-navy'
+                  }`}
+                />
+                {formErrors.name && (
+                  <p className="text-error">{formErrors.name}</p>
+                )}
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="john.doe@email.com"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  className={`rounded border p-2 ${
+                    touched.email && formErrors.email
+                      ? 'border-error bg-error-light' // Show error styling only if the field is touched
+                      : touched.email && !formErrors.email
+                        ? 'border-success bg-success-light' // Show success styling if touched and valid
+                        : 'border-navy'
+                  }`}
+                />
+                {formErrors.email && (
+                  <p className="text-error">{formErrors.email}</p>
+                )}
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="subject">Subject</label>
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  placeholder="Concerning order #12345"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  className={`rounded border p-2 ${
+                    touched.subject && formErrors.subject
+                      ? 'border-error bg-error-light' // Show error styling only if the field is touched
+                      : touched.subject && !formErrors.subject
+                        ? 'border-success bg-success-light' // Show success styling if touched and valid
+                        : 'border-navy'
+                  }`}
+                />
+                {formErrors.subject && (
+                  <p className="text-error">{formErrors.subject}</p>
+                )}
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder="Your message here..."
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  className={`h-28 rounded border p-2 ${
+                    touched.message && formErrors.message
+                      ? 'border-error bg-error-light' // Show error styling only if the field is touched
+                      : touched.message && !formErrors.message
+                        ? 'border-success bg-success-light' // Show success styling if touched and valid
+                        : 'border-navy'
+                  }`}
+                ></textarea>
+                {formErrors.message && (
+                  <p className="text-error">{formErrors.message}</p>
+                )}
+              </div>
+              <Button type="submit" style="primary">
+                Send Message
+              </Button>
             </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="john.doe@email.com"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                className={`rounded border p-2 ${
-                  touched.email && formErrors.email
-                    ? 'border-error bg-error-light' // Show error styling only if the field is touched
-                    : touched.email && !formErrors.email
-                      ? 'border-success bg-success-light' // Show success styling if touched and valid
-                      : 'border-navy'
-                }`}
-              />
-              {formErrors.email && (
-                <p className="text-error">{formErrors.email}</p>
-              )}
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="subject">Subject</label>
-              <input
-                type="text"
-                id="subject"
-                name="subject"
-                placeholder="Concerning order #12345"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                className={`rounded border p-2 ${
-                  touched.subject && formErrors.subject
-                    ? 'border-error bg-error-light' // Show error styling only if the field is touched
-                    : touched.subject && !formErrors.subject
-                      ? 'border-success bg-success-light' // Show success styling if touched and valid
-                      : 'border-navy'
-                }`}
-              />
-              {formErrors.subject && (
-                <p className="text-error">{formErrors.subject}</p>
-              )}
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="message">Message</label>
-              <textarea
-                id="message"
-                name="message"
-                placeholder="Your message here..."
-                onBlur={handleBlur}
-                onChange={handleChange}
-                className={`h-28 rounded border p-2 ${
-                  touched.message && formErrors.message
-                    ? 'border-error bg-error-light' // Show error styling only if the field is touched
-                    : touched.message && !formErrors.message
-                      ? 'border-success bg-success-light' // Show success styling if touched and valid
-                      : 'border-navy'
-                }`}
-              ></textarea>
-              {formErrors.message && (
-                <p className="text-error">{formErrors.message}</p>
-              )}
-            </div>
-            <Button type="submit" style="primary">
-              Send Message
+          </form>
+        )}
+        {/* Success message */}
+        {msgSent && (
+          <div className="mt-12 flex flex-col items-center gap-4">
+            <FontAwesomeIcon
+              icon={byPrefixAndName.fas['check-circle']}
+              className="text-9xl text-success"
+            />
+            <h3 className="text-4xl font-semibold text-navy">Message Sent</h3>
+            <p className="-mb-4">Thank you for reaching out to us!</p>
+            <p>We will get back to you as soon as possible.</p>
+            <Button style="primary" onClick={handleNavigation}>
+              Continue Shopping
             </Button>
           </div>
-        </form>
+        )}
       </section>
       {/* Contact details */}
       <section id="contact-details" className="bg-cat">
