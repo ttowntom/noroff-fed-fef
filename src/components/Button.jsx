b804d093ff4fd1a800c925d5c7e257489193ef1a
ttowntom <git.running054@passinbox.com>
2024-11-20 10:26:19 +0100

    adds disabled state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,17 @@
-export default function Button({ style, grow, inverse, children }) {
+export default function Button({ style, grow, inverse, children, ...props }) {
   let styling = 'border-navy rounded border px-4 py-2 hover:bg-cat ';
+
+  if (props.disabled) {
+    return (
+      <button
+        className="cursor-not-allowed rounded border !bg-gray-300 px-4 py-2 !text-gray-500 hover:!bg-gray-300"
+        disabled
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
 
   if (style === 'primary') {
     styling += ' bg-navy text-blue-zodiac-50 hover:text-navy';
@@ -14,5 +26,9 @@ export default function Button({ style, grow, inverse, children }) {
       ' bg-navy border border-white text-white hover:bg-white hover:text-navy';
   }
 
-  return <button className={styling}>{children}</button>;
+  return (
+    <button {...props} className={styling}>
+      {children}
+    </button>
+  );
 }
