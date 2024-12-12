91b181348375d6418be334d1f27fde6587caf86a
ttowntom <git.running054@passinbox.com>
2024-12-12 10:51:28 +0100

    refactor Callout component styles for improved readability

--- src/components/Callout.jsx
+++ src/components/Callout.jsx
@@ -1,7 +1,7 @@
 export default function Callout({ children }) {
   return (
-    <section className="bg-navy h-6 min-h-fit py-16 text-white">
-      <div className="text-blue-zodiac-50 mx-auto flex w-full max-w-screen-lg flex-col justify-center px-4 pb-4">
+    <section className="min-h-fit bg-navy py-16 text-white">
+      <div className="mx-auto flex w-full max-w-screen-lg flex-col justify-center px-4 pb-4 text-blue-zodiac-50">
         {children}
       </div>
     </section>
